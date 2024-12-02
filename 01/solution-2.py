64734a2fb75bda7009433cdef68808de7aea89ca
xdesign-chriswhitson <chris.whitson@xdesign.com>
2024-12-02 13:18:34 +0000

    renamed directories. streamlined day 1 puzzle 2

--- 01/solution-2.py
+++ 01/solution-2.py
@@ -6,9 +6,7 @@ with open(sys.argv[1]) as f:
     col1, col2 = list(zip(*pairs))
     counts1 = Counter(col1)
     counts2 = Counter(col2)
-    result = 0
-    for a,b in counts1.items():
-        result += a*b*counts2[a]
+    result = sum(a*b*counts2[a] for a,b in counts1.items())
     print(result)
     
     
